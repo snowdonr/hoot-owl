d46c16d1af793a3d97eb041a8b727a3c16d9e59f
snowdonr <72458484+snowdonr@users.noreply.github.com>
2021-11-27 15:17:41 -0700

    Create player.py

---
+++ src/player.py
@@ -0,0 +1,11 @@
+'''
+@author snowdonr
+'''
+
+class Player(object):
+  ''' State related to a single player position in the game, with a hand and strategy'''
+  def __init__(self):
+    pass
+  
+class Hand(object):
+  ''' '''
